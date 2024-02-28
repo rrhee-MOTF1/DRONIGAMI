e04f816b8c96debc7ad250702819b6098f9a817a
rrhee-MOTF1 <rrhee@bcfdmo.com>
2024-02-28 11:48:55 -0600

    Committing main repo

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
 @echo off
 call C:\DRONIGAMI\setup_files\setup.bat
-call C:\DRONIGAMI\setup_files\install_req.bat
+call C:\DRONIGAMI\setup_files\install_pandas.bat
+call C:\DRONIGAMI\setup_files\install_flask.bat
+call C:\DRONIGAMI\setup_files\install_waitress.bat
 schtasks /create /sc onlogon /tn DRONIGAMI_RUN /tr C:\DRONIGAMI\Run_App.bat
--- setup_files/install_pandas.bat
+++ setup_files/install_pandas.bat
@@ -1,5 +1,4 @@
 @echo off
 call C:\ProgramData\miniconda3\Scripts\activate.bat C:\ProgramData\miniconda3\envs\USaR_DRONIGAMI
-conda install flask -y --force-reinstall
-conda install waitress -y --force-reinstall
+conda install pandas -y --force-reinstall
 conda deactivate
