a3556aca71ecb8907084036ce0a725b231f1ca9a
rrhee-MOTF1 <rrhee@bcfdmo.com>
2024-02-28 11:58:49 -0600

    Committing main repo

--- install.bat
+++ install.bat
@@ -3,4 +3,4 @@ call C:\DRONIGAMI\setup_files\setup.bat
 call C:\DRONIGAMI\setup_files\install_pandas.bat
 call C:\DRONIGAMI\setup_files\install_flask.bat
 call C:\DRONIGAMI\setup_files\install_waitress.bat
-schtasks /create /sc onlogon /tn DRONIGAMI_RUN /tr C:\DRONIGAMI\Run_App.bat
+schtasks /create /sc onlogon /tn DRONIGAMI_RUN /tr C:\DRONIGAMI\Run_App - Shortcut.lnk
