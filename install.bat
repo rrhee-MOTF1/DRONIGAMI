f3bace9e2dd663cb24b24a6aa687ff80b79a92a2
rrhee-MOTF1 <rrhee@bcfdmo.com>
2024-02-28 10:54:16 -0600

    Committing main repo

--- install.bat
+++ install.bat
@@ -1,5 +1,4 @@
 @echo off
 call C:\DRONIGAMI\setup_files\setup.bat
-call C:\ProgramData\miniconda3\Scripts\activate.bat C:\ProgramData\miniconda3\envs\USaR_DRONIGAMI
 call C:\DRONIGAMI\setup_files\install_req.bat
 schtasks /create /sc onlogon /tn DRONIGAMI_RUN /tr C:\DRONIGAMI\Run_App.bat
