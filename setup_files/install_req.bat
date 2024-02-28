1ad97a3e3d598e277f7484c58b5d85e8b4374164
rrhee-MOTF1 <rrhee@bcfdmo.com>
2024-02-28 11:17:59 -0600

    Committing main repo

--- setup_files/install_req.bat
+++ setup_files/install_req.bat
@@ -1,4 +1,6 @@
 @echo off
 call C:\ProgramData\miniconda3\Scripts\activate.bat C:\ProgramData\miniconda3\envs\USaR_DRONIGAMI
-conda install conda-forge::pandas flask waitress -y --force-reinstall
+conda install conda-forge::pandas -y --force-reinstall
+conda install conda-forge::flask -y --force-reinstall
+conda install conda-forge::waitress -y --force-reinstall
 conda deactivate
